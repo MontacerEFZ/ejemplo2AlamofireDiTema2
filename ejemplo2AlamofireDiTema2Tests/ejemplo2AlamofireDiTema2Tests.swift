1b4b9bb35aaf5e521dd5b9001a42537a25d3b92b
MontacerElFazazi <montacer.elfazazielfazazi@cieep.com>
2023-12-21 10:23:32 +0100

    Initial Commit

---
+++ ejemplo2AlamofireDiTema2Tests/ejemplo2AlamofireDiTema2Tests.swift
@@ -0,0 +1,33 @@
+//
+//  ejemplo2AlamofireDiTema2Tests.swift
+//  ejemplo2AlamofireDiTema2Tests
+//
+//  Created by Montacer El Fazazi on 21/12/2023.
+//
+
+import XCTest
+@testable import ejemplo2AlamofireDiTema2
+
+class ejemplo2AlamofireDiTema2Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
